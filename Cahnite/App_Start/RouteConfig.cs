65f5382d466644725a5a4bd65a805c0f4d211483
Kurt Strecker <ksphotopro@gmail.com>
2018-06-24 23:28:00 -0400

    initial commit

--- Cahnite/App_Start/RouteConfig.cs
+++ Cahnite/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Cahnite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Projects", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
