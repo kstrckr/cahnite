73beb5f61e0fc32230d302134e36cd459200a09b
Kurt Strecker <ksphotopro@gmail.com>
2018-07-18 15:54:45 -0400

    added required to string values of all models

--- Cahnite/Controllers/ProjectsController.cs
+++ Cahnite/Controllers/ProjectsController.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
-
 using Cahnite.Models;
 
 namespace Cahnite.Controllers
--- Cahnite/Models/Project.cs
+++ Cahnite/Models/Project.cs
@@ -1,16 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 
 namespace Cahnite.Models
 {
     public class Project
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
-        public string Intro { get; set; }        
+        [Required]
+        public string Intro { get; set; }
+        [Required]
         [System.Web.Mvc.AllowHtml]
         public string BodyHtml { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
         public bool Favorite { get; set; }
     }
 
